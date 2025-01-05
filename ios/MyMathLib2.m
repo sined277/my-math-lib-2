a8ae87c2e208e327fe4b3dafa6c9a446bb36be93
vLomakaWork <vLomakaWork@Vitaliis-MacBook-Pro.local>
2025-01-05 16:19:44 +0200

    feat: initial commit with math library 2

--- ios/MyMathLib2.m
+++ ios/MyMathLib2.m
@@ -2,8 +2,24 @@
 
 @interface RCT_EXTERN_MODULE(MyMathLib2, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
+RCT_EXTERN_METHOD(multiply:(float)a 
+                  withB:(float)b
+                  withResolver:(RCTPromiseResolveBlock)resolve 
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(add:(float)a 
+                 withB:(float)b
+                 withResolver:(RCTPromiseResolveBlock)resolve 
                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(subtract:(float)a 
+                    withB:(float)b 
+                    withResolver:(RCTPromiseResolveBlock)resolve 
+                    withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(divide:(float)a 
+                  withB:(float)b 
+                  withResolver:(RCTPromiseResolveBlock)resolve 
+                  withRejecter:(RCTPromiseRejectBlock)reject)
 
 @end
